c33e8f1216c79636510c8a85332702b1cd96d220
gabri64n <gabrielrcastillo64hotmail.com>
2023-03-04 13:18:04 -0600

    Se agrega el Proyecto

---
+++ script.js
@@ -0,0 +1,157 @@
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+    });
+});
+
+const autoSlide = () => {
+    // if there is no image left to scroll then return from here
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+
+    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+    let firstImgWidth = firstImg.clientWidth + 14;
+    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    let valDifference = firstImgWidth - positionDiff;
+
+    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    // if user is scrolling to the left
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+
+const dragStart = (e) => {
+    // updatating global variables value on mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    // scrolling images/carousel to left according to mouse pointer
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
+
+window.onload = () => {
+    let links = document.querySelectorAll('.conf-menu')
+    
+    let primero = links[0]
+    primero.addEventListener('click', () => {
+      scrollSuave('#sobre-mi', 500, 78)
+    })
+  
+    let segundo = links[1]
+    segundo.addEventListener('click', () => {
+      scrollSuave('#cover', 500, 78)
+    })
+  
+    let tercero = links[2]
+    tercero.addEventListener('click', () => {
+      scrollSuave('#servicios', 500, 78)
+    })
+
+    let cuarto = links[3]
+    cuarto.addEventListener('click', () => {
+      scrollSuave('#por', 500, 78)
+    })
+
+    let quinto = links[4]
+    quinto.addEventListener('click', () => {
+      scrollSuave('#blog', 500, 78)
+    })
+
+    let sexto = links[5]
+    sexto.addEventListener('click', () => {
+      scrollSuave('#contacto', 500, 78)
+    })
+  }
+
+  let links = document.querySelectorAll('.titular')
+    
+  let primero = links[0]
+  primero.addEventListener('click', () => {
+    scrollSuave('#cover', 500, 78)
+  })
+  
+  
+  const scrollSuave = (objetivo, duracion, compensacion) => {
+    let elemObj = document.querySelector(objetivo)
+    let elemPos = elemObj.getBoundingClientRect().top - compensacion
+    let posInicial = window.pageYOffset
+    let tiempoInicial = null
+  
+    const animacion = tiempoAhora => {
+      if (tiempoInicial === null) tiempoInicial = tiempoAhora
+      tiempoPasado = tiempoAhora - tiempoInicial
+      let auxAnimacion = easeInOutQuad(tiempoPasado, posInicial, elemPos, duracion)
+      window.scrollTo(0, auxAnimacion)
+      if (tiempoPasado < duracion) requestAnimationFrame(animacion)
+    }
+    requestAnimationFrame(animacion)
+  }
+  
+  const easeInOutQuad = (t, b, c, d) => {
+    t /= d / 2
+    if (t < 1) return c / 2 * t * t + b
+    t--
+    return - c / 2 * (t * (t - 2) - 1) + b
+  }
+
+
+  let mybutton = document.getElementById("myBtn");
+      
+      
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
