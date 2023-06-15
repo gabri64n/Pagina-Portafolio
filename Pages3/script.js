3c85c0dc03925e981de978a7a6e6a54e32693c5a
gabri64n <gabrielrcastillo64hotmail.com>
2023-06-14 23:04:38 -0600

    hi

---
+++ Pages3/script.js
@@ -0,0 +1,58 @@
+//Animaciones
+window.sr= ScrollReveal();
+
+sr.reveal('.container__card', {
+	duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+
+//Tarjeta
+
+
+//buton
+  let mybutton = document.getElementById("myBtn");
+      
+      
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+//
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
